95127c68f45899fc91a54b25ad7f64f0b2e589b6
Yashwanth R <chawat@ieee.org>
2017-05-23 19:54:34 +0530

    first commit

---
+++ TicTacToe/index.js
@@ -0,0 +1,119 @@
+var chance = 0;
+var player1 = document.querySelector('#player1');
+var player2 = document.querySelector('#player2');
+var s1 = document.querySelector('#scorep1');
+var s2 = document.querySelector('#scorep2');
+var score1 = 0;
+var score2 = 0;
+var cell1 = document.querySelector('#c1');
+var cell2 = document.querySelector('#c2');
+var cell3 = document.querySelector('#c3');
+var cell4 = document.querySelector('#c4');
+var cell5 = document.querySelector('#c5');
+var cell6 = document.querySelector('#c6');
+var cell7 = document.querySelector('#c7');
+var cell8 = document.querySelector('#c8');
+var cell9 = document.querySelector('#c9');
+var resultp = document.querySelector('#result');
+var resetarr = [cell1 , cell2 , cell3 , cell4 , cell5 , cell6 , cell7 , cell8 , cell9];
+function entry(div) {
+        check();
+        if (chance % 2 === 0) {
+            if (div.innerHTML !== "O" || div.innerHTML !== "X") {
+                div.innerHTML = "O";
+                chance++;
+            }
+        } else {
+            if (div.innerHTML !== "O" || div.innerHTML !== "X") {
+                div.innerHTML = "X";
+                chance++;
+            }
+        }
+        check();
+}
+
+function check() {
+    if (cell1.innerHTML === cell2.innerHTML && cell1.innerHTML === cell3.innerHTML) {
+        if (cell1.innerHTML === "O") {
+            resultp.innerHTML = player1.innerHTML + "Wins !";
+            s1.innerHTML = "Score: " + ++score1;
+            return;
+        } else if (cell1.innerHTML === "X") {
+            resultp.innerHTML = player2.innerHTML + "Wins !";
+            s2.innerHTML = "Score: " + ++score2;
+            return;
+        }
+    } else if (cell1.innerHTML === cell4.innerHTML && cell1.innerHTML === cell7.innerHTML) {
+        if (cell1.innerHTML === "O") {
+            resultp.innerHTML = player1.innerHTML + "Wins !";
+            s1.innerHTML = "Score: " + ++score1;
+            return;
+        } else if (cell1.innerHTML === "X") {
+            resultp.innerHTML = player2.innerHTML + "Wins !";
+            s2.innerHTML = "Score: " + ++score2;
+            return;
+        }
+    } else if (cell1.innerHTML === cell5.innerHTML && cell1.innerHTML === cell9.innerHTML) {
+        if (cell1.innerHTML === "O") {
+            resultp.innerHTML = player1.innerHTML + "Wins !";
+            s1.innerHTML = "Score: " + ++score1;
+            return;
+        } else if (cell1.innerHTML === "X") {
+            resultp.innerHTML = player2.innerHTML + "Wins !";
+            s2.innerHTML = "Score: " + ++score2;
+            return;
+        }
+    } else if (cell7.innerHTML === cell8.innerHTML && cell7.innerHTML === cell9.innerHTML) {
+        if (cell7.innerHTML === "O") {
+            resultp.innerHTML = player1.innerHTML + "Wins !";
+            s1.innerHTML = "Score: " + ++score1;
+            return;
+        } else if (cell7.innerHTML === "X") {
+            resultp.innerHTML = player2.innerHTML + "Wins !";
+            s2.innerHTML = "Score: " + ++score2;
+            return;
+        }
+    } else if (cell3.innerHTML === cell6.innerHTML && cell3.innerHTML === cell9.innerHTML) {
+        if (cell3.innerHTML === "O") {
+            resultp.innerHTML = player1.innerHTML + "Wins !";
+            s1.innerHTML = "Score: " + ++score1;
+            return;
+        } else if (cell3.innerHTML === "X") {
+            resultp.innerHTML = player2.innerHTML + "Wins !";
+            s2.innerHTML = "Score: " + ++score2;
+            return;
+        }
+    } else if (cell4.innerHTML === cell5.innerHTML && cell4.innerHTML === cell6.innerHTML) {
+        if (cell4.innerHTML === "O") {
+            resultp.innerHTML = player1.innerHTML + "Wins !";
+            s1.innerHTML = "Score: " + ++score1;
+            return;
+        } else if (cell4.innerHTML === "X") {
+            resultp.innerHTML = player2.innerHTML + "Wins !";
+            s2.innerHTML = "Score: " + ++score2;
+            return;
+        }
+    } else if (cell2.innerHTML === cell5.innerHTML && cell2.innerHTML === cell8.innerHTML) {
+        if (cell2.innerHTML === "O") {
+            resultp.innerHTML = player1.innerHTML + "Wins !";
+            s1.innerHTML = "Score: " + ++score1;
+            return;
+        } else if (cell2.innerHTML === "X") {
+            resultp.innerHTML = player2.innerHTML + "Wins !";
+            s2.innerHTML = "Score: " + ++score2;
+            return;
+        }
+    }
+    if (chance === 9) {
+        resultp.innerHTML = "It's a Tie!";
+    }
+}
+    function reset() {
+        for (var i = 0; i < resetarr.length; i++) {
+            resetarr[i].innerHTML = "";
+        }
+        resultp.innerHTML = "";
+        chance = 0;
+        return;
+    }
+
