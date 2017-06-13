51022c131e182da407614e9ac223fb17cd93dc6c
Yashwanth R <chawatyash@gmail.com>
2017-06-13 17:24:46 +0530

    Add files via upload

---
+++ ImageViewer/index.js
@@ -0,0 +1,20 @@
+var left = document.querySelector('#left');
+var right = document.querySelector('#right');
+var imgs = ['1.jpeg' , '2.jpg' , '3.jpg'];
+var bg = document.querySelector('#img');
+var i = 0;
+left.addEventListener('click' , movePre);
+right.addEventListener('click' , moveFwd);
+
+
+function moveFwd() {
+  i++;
+//  bg.style.backgroundImage = imgs[i%3];
+  bg.style.backgroundImage = "url(" + imgs[i%3].toString() +")";
+}
+
+function movePre() {
+  i--;
+  bg.style.backgroundImage = "url(" + imgs[i%3].toString() +")";
+  // bg.style.backgroundImage = transparent;
+}
